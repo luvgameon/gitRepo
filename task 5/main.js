2c240372fa68dfca51dcd757a11bb6b6fd53e50a
luvgameon <110060269+luvgameon@users.noreply.github.com>
2022-11-17 21:22:07 +0530

    task 5 call by tag name

---
+++ task 5/main.js
@@ -0,0 +1,16 @@
+// call by tagname 
+var item=document.getElementsByTagName('li');
+item[1].innerText='Gameon';
+item[1].style.fontWeight="bold";
+item[1].style.backgroundColor="green";
+for(var i=0;i<item.length;i++)
+{
+    item[i].style.backgroundColor='#f4f4f4';
+}
+item[1].style.backgroundColor="green";
+
+
+
+
+
+               
