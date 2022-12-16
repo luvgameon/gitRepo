f7efc04cce7aac21a99cbe2c835d4dd90119ab0a
luvgameon <110060269+luvgameon@users.noreply.github.com>
2022-12-16 16:47:35 +0530

    Update test.js

--- expense tracker/test.js
+++ expense tracker/test.js
@@ -1,20 +1,35 @@
-let btn=document.querySelector('#btn').addEventListener('click',()=>{
-    let amount=document.querySelector('#rs').value;
-    let des=document.querySelector('#des').value;
-    let cat=document.querySelector('#cat').value;
 
-    const parentNode = document.getElementById('myul');
-                const childHTML = `<li id=${amount}> ${des} - ${cat}
-                                        <button onclick=deleteUser('${}')> Delete User </button>
-                                        <button onclick=editUserDetails('${user.email}','${user.name}','${user.phonenumber}')>Edit User </button>
-                                     </li>`
-
-                parentNode.innerHTML = parentNode.innerHTML + childHTML;
-            }
+let btn=document.getElementById('btn').addEventListener('click',()=>{
 
 
+    let amount=document.getElementById('rs').value;
+    let des=document.getElementById('des').value;
+    let cat=document.getElementById('cat').value;
+    const parentNode = document.getElementById('ul');
+    const childHTML = `<li id=${amount} ><strong>Amount is</strong>  : ${amount} <strong>Description is </strong>: ${des}  <strong>Category is</strong> : ${cat}
+                            <button onclick=deleteUser('${amount}')> Delete User </button>
+                            <button onclick=editUserDetails('${amount}','${des}','${cat}')>Edit User </button>
+                         </li>
+                         `
+                         
 
-    
-
-
+    parentNode.innerHTML = parentNode.innerHTML + childHTML;
 })
+
+function deleteUser(desId){
+    console.log(desId)
+    const parentNode = document.getElementById('ul');
+                const childNodeToBeDeleted = document.getElementById(desId);
+                if(childNodeToBeDeleted) {
+                    parentNode.removeChild(childNodeToBeDeleted)
+                }
+}
+
+function editUserDetails(desId, name, cat){
+
+    document.getElementById('rs').value = desId;
+    document.getElementById('des').value = name;
+    document.getElementById('cat').value =cat;
+
+    deleteUser(desId)
+ }
